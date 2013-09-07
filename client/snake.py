93098118e6a605f6171c8fed7db262fe54b51272
Logan Zhou <xorcererzc@gmail.com>
2013-09-08 02:39:08 +0800

    Create client with kivy. Board done.

--- client/snake.py
+++ client/snake.py
@@ -1,72 +1,28 @@
-""" A console client, donated by fancyzero, would be emoved later for it does not work well."""
-import sys
-import os
-import time
-import socket
-import json
-import string
-mapw = 40
-maph = 40
-snakeskin=["*","$","#","%","~"]
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
 
-mapdata=""
-def tick():
-    global s
-    global mapdata
-    #send command
-    command = '{"action":"new_snake"}\r\n'
-    s.send(command)
-    #read socket
-    while(True):
-        mapdata = mapdata + s.recv(10)
-        #see if we got a correct snapshot of map
-        index = string.find(mapdata,"\r\n")
-        if index >= 0:
-            print index
-            map_snap = mapdata[0:index]
-            mapdata = mapdata[index+2:]
-            break
 
-    #initial map grids
-    map_view = []
-    for i in range(mapw*maph):
-        map_view.append(" ")
+class SquareWidget(Widget):
+    pass
 
-    #parse map snap
-    parsed_map = json.loads(map_snap)
-    for food in parsed_map["map"]:
-        pos = eval('[' + food + ']')
-        map_view[pos[0]+pos[1]*mapw]="O"
+class BoardWidget(GridLayout):
+    def __init__(self, rows, cols):
+        super(self.__class__, self).__init__()
 
-    for key in parsed_map["snakes"]:
-        bodys = parsed_map["snakes"][key]
-        for body in bodys:
-            pos = eval('[' + body + ']')
-            map_view[pos[0]+pos[1]*mapw]="*"
+        self.rows = rows
+        self.cols = cols
 
-    #clear screen
-    os.system('cls' if os.name=='nt' else 'clear')
+        for x in xrange(self.rows * self.cols):
+            self.add_widget(SquareWidget())
 
-    #draw map view
-    for i in range(mapw):
-        for j in range(maph):
-           sys.stdout.write(map_view[i+j*mapw])
-        sys.stdout.write("\r\n")
+class SnakeApp(App):
 
-def main():
-    global s
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if len(sys.argv) > 2:
-        server_addr = sys.argv[1]
-        server_port = sys.argv[2]
-    else:
-        server_addr = "localhost"
-        server_port = 10080
+    def build(self):
+        board = BoardWidget(10, 10)
 
-    s.connect( (server_addr, int(server_port)))
-    print "connected to:", server_addr, server_port
-    while(True):
-        time.sleep(0.5)
-        tick()
+        return board
 
-main()
+
+if __name__ == '__main__':
+    SnakeApp().run()
